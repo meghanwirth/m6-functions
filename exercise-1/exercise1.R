e34d3ee851933ecd9e4fbd587517537e9e697431
Meghan Wirth <megznw@D-140-142-143-47.dhcp4.washington.edu>
2016-10-06 15:19:07 -0700

    participation

---
+++ exercise-1/exercise1.R
@@ -0,0 +1,34 @@
+# Exercise-1: practice with basic syntax
+
+# Create a variable `hometown` that stores the city in which you were born
+hometown <- 'corvallis'
+# Assign your name to the variable `my.name`
+my.name <- 'Meghan'
+# Assign your height to a variable `my.height`
+my.height <- 'five foot three inches'
+# Create a variable `puppies` equal to the number of puppies you'd like to have
+puppies <- '2'
+# Create a variable `puppy.price`, which is how expensive you think a puppy is
+puppy.price <- '400'
+# Create a variable `total.cost` that has the total cost of all of your puppies
+
+# Create a boolean variable `too.expensive`, set to true if the cost is greater than $1,000
+
+# Create a variable `max.puppies`, which is the nuber of puppies you can afford for $1K.
+
+hometown <- Corvallis
+'hometown' <- Corvallis
+hometown <- 'Corvallis'
+
+total.cost <- '800'
+too.expensive <- 800 > 1000
+max.puppies <- '2'
+# Write a function to add two numbers together
+AddNumbers <- function(a, b) {   
+  # Function body: perform tasks in here   
+  answer <- a + b
+# Return statement: what you want the function to output   return (answer) }
+a + B
+AddNumbers <- function (a,b)
+answer <- a + b
+return (answer)
